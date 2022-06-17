c265bcb6c36f6a9be598a13f05437310ff6dae12
Lucas <oligena@hotmail.com>
2022-06-17 15:34:11 -0300

    added not found exception

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,21 +1,33 @@
-import { Controller, Get, Post, Body, Param, Query } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, NotFoundException } from '@nestjs/common';
 import { CreateMessageDto } from './dtos/create-message-dto'
+import { MessagesService } from './messages.service'
 
 @Controller('messages')
 export class MessagesController {
+  messagesService: MessagesService
+
+  constructor() {
+
+    this.messagesService = new MessagesService()
+  }
+
   @Get()
   listMessages() {
-    console.log('get')
+    return this.messagesService.findAll()
   }
 
   @Post()
   createMessage(@Body() body: CreateMessageDto) {
     const { content } = body
-    console.log(content)
+    return this.messagesService.create(content)
   }
 
   @Get('/:id')
-  getMessage(@Param() id: String) {
-    console.log(id)
+  async getMessage(@Param('id') id: string) {
+    const message = await this.messagesService.findOne(id)
+    if (!message) {
+      throw new NotFoundException('Message not found')
+    }
+    return message
   }
 }
